f168866037186ed333dedd7c90e4332a786d1bd5
Vladimir Danilevskiy <vladimir.danilevskiy@ispringsolutions.com>
2016-02-11 18:49:44 +0300

    заготовка для считывания команд

--- car/main.cpp
+++ car/main.cpp
@@ -1,8 +1,59 @@
 #include "stdafx.h"
 #include "Car.h"
 
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "Car.h"
 
-int main()
+void ProcessCommandLine(CCar & car, std::string & line, std::ostream & ostream);
+
+void ProcessInfoCommand(CCar & car, std::istringstream & istream, std::ostream & ostream);
+void ProcessEngineCommand(CCar & car, std::istringstream & istream, std::ostream & ostream);
+void ProcessSpeedCommand(CCar & car, std::istringstream & istream, std::ostream & ostream);
+void ProcessGearCommand(CCar & car, std::istringstream & istream, std::ostream & ostream);
+
+void main()
 {
-	return 0;
+	CCar car = CCar();
+	std::string line;
+	while (std::getline(std::cin, line))
+	{
+		ProcessCommandLine(car, line, std::cout);
+	}
+}
+
+void ProcessCommandLine(CCar & car, std::string & line, std::ostream & ostream)
+{
+	std::istringstream istream(line);
+
+	std::string command;
+
+	if (istream >> command)
+	{
+		if (command == "info")
+		{
+			ProcessInfoCommand(car, istream, std::cout);
+		}
+		else if (command == "engine")
+		{
+			ProcessEngineCommand(car, istream, std::cout);
+		}
+		else if (command == "speed")
+		{
+			ProcessSpeedCommand(car, istream, std::cout);
+		}
+		else if (command == "gear")
+		{
+			ProcessGearCommand(car, istream, std::cout);
+		}
+
+	}
+	else
+	{
+		ostream << "Command error" << std::endl;
+	}
+	ostream << std::endl;
+
 }
